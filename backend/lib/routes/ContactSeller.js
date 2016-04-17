dddbec214409a8bcc236dd166378dccf3e2da23a
Andrew <a.macmurray@icloud.com>
2016-04-17 13:46:08 +0100

    some clean up of home view, submit redirects to product page

--- backend/lib/routes/ContactSeller.js
+++ backend/lib/routes/ContactSeller.js
@@ -11,7 +11,8 @@ export default {
     auth: 'session',
     handler: (request, reply) => {
       const oauth = jwt.verify(request.auth.credentials.twitterCookie, JWT_SECRET)
-      const status = encodeURI('i really love reuseapp') // eslint-disable-line
+      const status = '%40reuseapp%20reuseapp' // eslint-disable-line
+      console.log(status)
       const url = 'https://api.twitter.com/1.1/statuses/update.json?status=' + status
       req.post({ url, oauth }, (err, response, body) => {
         reply(body)
