470d11edde5da9326780a0e4f14a5da1335d1ea0
spudtrader <jmurphy.web@gmail.com>
2016-04-17 15:40:16 +0100

    add user tweet

--- backend/lib/routes/ContactSeller.js
+++ backend/lib/routes/ContactSeller.js
@@ -6,15 +6,17 @@ const JWT_SECRET = process.env.JWT_SECRET
 
 export default {
   path: '/contact-seller',
-  method: 'GET',
+  method: 'POST',
   config: {
     auth: 'session',
     handler: (request, reply) => {
+      console.log(request.payload)
       const oauth = jwt.verify(request.auth.credentials.twitterCookie, JWT_SECRET)
-      const status = '%40reuseapp%20reuseapp' // eslint-disable-line
+      const status = '%40' + request.payload.userName + '%20' + encodeURI(request.payload.text) // eslint-disable-line
       console.log(status)
-      const url = 'https://api.twitter.com/1.1/statuses/update.json?status=' + status
+      const url = 'https://api.twitter.com/1.1/statuses/update.json?status=' + status + '&in_reply_to_status_id=' + request.payload.tweetId
       req.post({ url, oauth }, (err, response, body) => {
+        console.log(body)
         reply(body)
       })
     }
--- frontend/src/js/components/Item/index.js
+++ frontend/src/js/components/Item/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import { ButtonInput, Image, Input } from 'react-bootstrap'
 import { Link } from 'react-router'
+import axios from 'axios'
 
 export default class Item extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      formValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    console.log(e.target)
+    this.setState({ formValue: e.target.value })
+  }
+
   render () {
     const thisItem = this.props.items.filter(item => {
       return item.tweetId === this.props.id
@@ -15,10 +29,22 @@ export default class Item extends React.Component {
         {thisItem.place}
         {thisItem.provider.userName} <Image src={thisItem.provider.profileImage} circle responsive/>
 
-        <form onSubmit={'/contact-seller'}>
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          const options = {
+            text: this.state.formValue,
+            tweetId: thisItem.tweetId,
+            userName: thisItem.provider.userName
+          }
+          axios.post('/contact-seller', options).then(data => {
+            console.log(data)
+          })
+        }}
+        >
           <Input
             type='text'
-            value={this.state.value}
+            onChange={this.handleChange}
+            value={this.state.formValue}
             placeholder={'Send a tweet to ' + thisItem.provider.userName}
             label='Working example with validation'
             help='Validation is based on string length.'
@@ -33,15 +59,3 @@ export default class Item extends React.Component {
     )
   }
 }
-
-<div>
-        <input
-        ref={node => {this.input = node}}/>
-        <button
-        onClick={() => {
-          this.props.addTask(this.input.value)
-          this.input.value = ''
-        }}>
-          Add To Do
-        </button>
-      </div>
